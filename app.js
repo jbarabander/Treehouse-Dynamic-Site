177ea773fc3f6b579dc9826568f468a354f29bbc
Justin Barabander <justinbarabander@Justins-MBP.home>
2015-07-24 09:57:15 -0400

    Completed the website

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var router = require('./router.js');
+
 //Problem: We need a simple way to look at a user's badge count and Javascript points from a web browser
 //Solution: Use Node.js to perform the profile look ups and server our templates via HTTP
 
